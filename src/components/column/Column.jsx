944fa6bc6c1fe8d1d3d1333ef21526f3b0c10f56
Hardy Lutula <hardylutula@gmail.com>
2023-04-28 04:13:11 +0200

    addTask added

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,11 +1,26 @@
 import './Column.css'
 import Task from '../task/Task'
+import {useStore} from '../../store'
+import {shallow} from 'zustand/shallow'
 
 const Column = ({state}) => {
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state == state),
+    shallow
+  )
+
+  const addTask = useStore((store) => store.addTask)
+
   return (
     <div className='column'>
-      <p>{state}</p>
-      <Task title='Todo' />
+      <div className='titleWrapper'>
+        <p>{state}</p>
+        <button onClick={() => addTask('Hey ' + state, state)}>Add</button>
+      </div>
+
+      {tasks.map((task) => (
+        <Task key={task.title} title={task.title} />
+      ))}
     </div>
   )
 }
