ab159c9a29abe406797c03d25db2892ae3c504c9
Hardy Lutula <hardylutula@gmail.com>
2023-04-29 06:16:12 +0200

    basic modal added

--- src/components/column/Column.jsx
+++ src/components/column/Column.jsx
@@ -1,9 +1,13 @@
+import {useState} from 'react'
 import './Column.css'
 import Task from '../task/Task'
 import {useStore} from '../../store'
 import {shallow} from 'zustand/shallow'
 
 const Column = ({state}) => {
+  const [text, setText] = useState('')
+  const [open, setOpen] = useState(false)
+
   const tasks = useStore(
     (store) => store.tasks.filter((task) => task.state == state),
     shallow
@@ -15,12 +19,33 @@ const Column = ({state}) => {
     <div className='column'>
       <div className='titleWrapper'>
         <p>{state}</p>
-        <button onClick={() => addTask('Hey ' + state, state)}>Add</button>
+        <button onClick={() => setOpen(true)}>Add</button>
       </div>
 
       {tasks.map((task) => (
         <Task key={task.title} title={task.title} />
       ))}
+
+      {open && (
+        <div className='modal'>
+          <div className='modalContent'>
+            <input
+              type='text'
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                addTask(text, state)
+                setText('')
+                setOpen(false)
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
