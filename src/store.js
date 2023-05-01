c1351493ac9e4f48818e8b9669764ae6b0b2868a
hardy <hardy@lutula.com>
2023-05-02 01:28:50 +0200

    added the persist middleware

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {create} from 'zustand'
-import {devtools} from 'zustand/middleware'
+import {devtools, persist} from 'zustand/middleware'
 
 const store = (set) => ({
   tasks: [{id: 770271739812, title: 'TestTask', state: 'ONGOING'}],
@@ -7,21 +7,34 @@ const store = (set) => ({
   draggedTask: null,
 
   addTask: (title, state) =>
-    set((store) => ({tasks: [...store.tasks, {id: Date.now(), title, state}]})),
+    set(
+      (store) => ({tasks: [...store.tasks, {id: Date.now(), title, state}]}),
+      false,
+      'addTask'
+    ),
 
   deleteTask: (id) =>
-    set((store) => ({
-      tasks: store.tasks.filter((task) => task.id !== id),
-    })),
+    set(
+      (store) => ({
+        tasks: store.tasks.filter((task) => task.id !== id),
+      }),
+      false,
+      'deleteTask'
+    ),
 
-  setDraggedTask: (id, title) => set({draggedTask: {id, title}}),
+  setDraggedTask: (id, title) =>
+    set({draggedTask: {id, title}}, false, 'setDraggedTask'),
 
   moveTask: (id, title, state) =>
-    set((store) => ({
-      tasks: store.tasks.map((task) =>
-        task.title === title ? {id, title, state} : task
-      ),
-    })),
+    set(
+      (store) => ({
+        tasks: store.tasks.map((task) =>
+          task.title === title ? {id, title, state} : task
+        ),
+      }),
+      false,
+      'moveTask'
+    ),
 })
 
-export const useStore = create(devtools(store))
+export const useStore = create(persist(devtools(store), {name: 'store'}))
